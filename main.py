4089036067a5a2f920bfebcc3c2c97ac9dc28b97
Peniel Nissi Obeng <p.obeng@alustudent.com>
2025-07-23 16:26:33 +0200

    Updated main.py

--- main.py
+++ main.py
@@ -30,6 +30,8 @@ def main():
         except ValueError:
             print("Invalid input. Please enter a number.")
 
+
+# Password verification helper
 def check_password(password: str, hashed: str) -> bool:
     return bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
 
@@ -37,6 +39,7 @@ def login():
     print("Welcome to our login page.")
     print("--------------------------")
 
+    # Email pattern matching
     pattern_student = r"^[a-zA-Z0-9._%+-]+@alustudent\.com$"
     pattern_education = r"^[a-zA-Z0-9._%+-]+@alueducation\.com$"
     pattern_admin = r"^[a-zA-Z0-9._%+-]+@aluadmin\.com$"
@@ -44,18 +47,21 @@ def login():
     email = input("Enter your email: ").strip()
     password = input("Enter your password: ")
 
-    conn = get_connection()
-    cursor = conn.cursor()
+    # Attempt to retrieve user record from DB
+    connect = get_connection()
+    cursor = connect.cursor()
     cursor.execute("SELECT password_hash, role FROM users WHERE email = %s", (email,))
     result = cursor.fetchone()
     cursor.close()
-    conn.close()
+    connect.close()
 
     if result:
         stored_hash, stored_role = result
         if check_password(password, stored_hash):
+            #  Password matched
             print(f" Login successful. Welcome {stored_role}!")
 
+            # Match email to expected role
             if re.match(pattern_student, email) and stored_role == "student":
                 menus.Student_menu()
             elif re.match(pattern_education, email) and stored_role == "instructor":
@@ -82,15 +88,15 @@ def sign_up():
     password= input("Enter your password: ")
     hashed_pw = hash_password(password)
 
-    conn = get_connection()
-    cursor = conn.cursor()
+    connect = get_connection()
+    cursor = connect.cursor()
     cursor.execute("""
         INSERT INTO pending_signups (full_name, email, password_hash)
         VALUES (%s, %s, %s)
     """, (full_name, email, hashed_pw))
-    conn.commit()
+    connect.commit()
     cursor.close()
-    conn.close()
+    connect.close()
 
     print(" Signup request submitted. Awaiting admin approval.")
 
